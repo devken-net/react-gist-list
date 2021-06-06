47857180f9fcbd233201a1a07e72b1f588361cad
Ken Ortiz <ken@opsmanager.com>
2021-06-06 22:33:14 +0800

    add search header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
+import { Input, Row, Col, Card } from 'antd';
 import './App.css';
 
+const { Search } = Input;
+
+
 function App() {
+  const onSearch = (username) => { console.log(username) }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <h1 className="text-center">GithubGist</h1>
+        <div className="Search pa-4">
+          <Row align="middle" justify="center">
+            <Col flex="none" className="pr-4">
+              Search
+            </Col>
+            <Col flex="auto">
+              <Search
+                placeholder="username"
+                allowClear
+                enterButton="Search"
+                size="large"
+                onSearch={onSearch}
+              />
+            </Col>
+          </Row>
+        </div>
       </header>
     </div>
   );
