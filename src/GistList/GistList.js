2ee090fec18016903aff47f0d9a75c3cdbd5dc6f
Ken Ortiz <ken@opsmanager.com>
2021-06-07 16:25:01 +0800

    add forks avatar and link

--- src/GistList/GistList.js
+++ src/GistList/GistList.js
@@ -1,4 +1,4 @@
-import { Button, Col, Collapse, Row } from 'antd';
+import { Avatar, Button, Col, Collapse, Row, Tooltip } from 'antd';
 import { useDispatch, useSelector } from "react-redux";
 import { getAllGistForks } from './GistListAction';
 import './GistList.css';
@@ -11,9 +11,25 @@ function GistListComponent() {
 
   const dispatch = useDispatch();
   const onCollapsed = (key) => {
-    console.log(gistList[key]);
     const { id, forks_url } = gistList[key];
     dispatch(getAllGistForks(id, forks_url));
+  }
+
+  const ForkedBy = (forks) => {
+    if (!forks) return;
+    const lastThreeForks = forks.slice(-3, forks.length);
+
+    return <>
+      {
+        lastThreeForks.map(fork => 
+          <Tooltip title={fork?.owner?.login} placement="top" className="mx-2">
+            <Button type="link" href={fork?.html_url} target="_blank">
+              <Avatar src={ fork?.owner?.avatar_url } />
+            </Button>
+          </Tooltip>
+        )
+      }
+    </>
   }
 
   return (
@@ -31,12 +47,27 @@ function GistListComponent() {
             >
               <Row align="middle" justify="start" gutter="12">
                 <Col flex="none" className="pr-4">
-                  {gist?.owner?.login} / { gist?.files && Object.keys(gist.files)[0] }
+                  { gist?.description || 'No available description' }
                 </Col>
               </Row>
               <Row align="middle" justify="start" gutter="12">
-                <Col flex="none" className="pr-4">
-                  Forks: { gist?.forks?.length }
+                <Col flex="none" className="pr-4"> Forks: </Col>
+                <Col flex="auto" className="pr-4"> { gist?.forks?.length } </Col>
+              </Row>
+              <Row align="middle" justify="start" gutter="12">
+                <Col flex="none" className="pr-4"> Forked By: </Col>
+                <Col flex="auto" className="pr-4">
+                  {
+                     ForkedBy(gist?.forks)
+                  }
+                </Col>
+              </Row>
+              <Row align="middle" justify="start" gutter="12">
+                <Col flex="none" className="pr-4"> Files: </Col>
+                <Col flex="auto" className="pr-4">
+                  {
+                     ForkedBy(gist?.forks)
+                  }
                 </Col>
               </Row>
             </Panel>
